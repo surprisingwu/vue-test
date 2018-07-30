39e69787fef5de0249383084570cee88b4833972
wyt <1348512234@qq.com>
2018-07-30 18:27:00 +0800

    init test

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,14 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'semi': 0,
+    'indent': 0,
+    'no-underscore-dangle': 0,
+    'import/extensions': ['off', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from 'vue'
+import App from './App'
 
-Vue.config.productionTip = false;
-
-new Vue({
-  router,
-  store,
+/* eslint-disable */
+const app = new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app');
+})
